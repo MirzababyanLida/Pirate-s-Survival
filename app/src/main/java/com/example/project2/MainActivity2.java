192a96a0e1cb8723bfed185778105603d05ca760
MirzababyanLida <lidamirzababyan@gmail.com>
2024-02-09 21:43:40 +0400

    Pirate's Survival info

---
+++ app/src/main/java/com/example/project2/MainActivity2.java
@@ -0,0 +1,31 @@
+package com.example.project2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.project2.R;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main4);
+        Button button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
