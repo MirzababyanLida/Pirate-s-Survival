73fc84da384eae429874c84c60487add9980c4d6
MirzababyanLida <lidamirzababyan@gmail.com>
2024-02-23 21:06:34 +0400

    mini game

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.project2;
 
-import com.example.project2.R;
+
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
-import android.widget.Button;
+
+
 
 
 
